1d13751c9d61cba5a106bff7f8ff98f99b3b415d
tor <victor.matheron@gmail.com>
2021-02-16 14:35:11 +0100

    First Commit : Grid Layout working + LightBox + api folder created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+const SOURCES = [
+  {title: "Image 1", src: "images/img-1.jpg"},
+  {title: "Image 2", src: "images/img-2.jpg"},
+  {title: "Image 3", src: "images/img-3.jpg"},
+  {title: "Image 4", src: "images/img-4.jpg"},
+  {title: "Image 5", src: "images/img-5.jpg"},
+  {title: "Image 6", src: "images/img-6.jpg"},
+  {title: "Image 7", src: "images/img-7.jpg"},
+  {title: "Image 8", src: "images/img-8.jpg"},
+  {title: "Image 9", src: "images/img-9.jpg"},
+  {title: "Image 10", src: "images/img-10.png"},
+  {title: "Image 11", src: "images/photographer.jpg"}
+]
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App sources={SOURCES}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
