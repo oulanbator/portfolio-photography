07224ff519764f24723c2a83e8b8c86482a88e82
tor <victor.matheron@gmail.com>
2021-02-18 01:34:54 +0100

    App avec un serveur express en prod, début de navigation mis en place

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="Contact">
+        This is the contact page.
+      </div>
+    )
+  }
+}
+export default Contact;
---
+++ src/gallery.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// import './styles.css'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App galleryTitle="Mariages"/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
